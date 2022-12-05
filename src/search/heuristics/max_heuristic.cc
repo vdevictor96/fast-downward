30df196a3b4bc0c81e90b82eac10c7c190e70730
Magnus <macu00001@stud.uni-saarland.de>
2022-12-05 22:31:28 +0100

    Version does not work but includes the complete layer implementation

--- src/search/heuristics/max_heuristic.cc
+++ src/search/heuristics/max_heuristic.cc
@@ -16,39 +16,53 @@ using namespace std;
 
 vector<int> counter;
 map <pair<int, int>, int> fac_mem_layer;
+map <pair<int, int>, int> goal_mem_map;
 vector <int> action_mem_layer;
 queue <pair<int, int>> fact_schedule;
 queue <int> action_schedule;
 int timestep;
-int req_goal; 
+int req_goal;
 vector <int> goal_mem_layer;
 vector <bool> operator_mark; /*Check if this is necessary. This should be redundant with the .mark() operation*/
 
-MaxHeuristic::MaxHeuristic(const Options &opts)
+struct hashFunction
+{
+    size_t operator()(const pair<int,
+        int>& x) const
+    {
+        return x.first ^ x.second;
+    }
+};
+unordered_set <pair<int, int>, hashFunction> goal_set;
+
+MaxHeuristic::MaxHeuristic(const Options& opts)
     : Heuristic(opts) {
 }
 
 
 void MaxHeuristic::initialize() {
-    cout << "Initializing max heuristic..." << endl; 
+    cout << "Initializing max heuristic..." << endl;
+    /*Init goal set to quickly find out whether an achieved fact is a goal fact*/
+    for (int i = 0; i < g_goal.size(); ++i) {
+        goal_set.insert(g_goal[i]);
+    }
 
- 
+
     /*Try to get a better way of initializing the vector*/
 }
 
-static void goal_fact(pair<int, int> fact) {
+
+
+static void goal_fact(pair<int, int>& fact) {
     /*Check if an (var,val) is an goal fact. Quite inefficient right now*/
-    for (int g_ind = 0; g_ind < g_goal.size(); ++g_ind) {
-        if (goal_mem_layer[g_ind] == -1) {
-            if (g_goal[g_ind].first == fact.first && g_goal[g_ind].second == fact.second) {
-                goal_mem_layer[g_ind] = timestep;
-                /*cout << fact.first << " " << fact.second << endl;*/
-                req_goal--;
-            }
-
+    if (goal_mem_map.find(fact) != goal_mem_map.end()) {
+        if (goal_set.find(fact) != goal_set.end()) {
+            goal_mem_map.insert(make_pair(fact, timestep));
+            req_goal--;
         }
-
     }
+
+
 }
 
 static void init_layers_and_counter(const State& state) {
@@ -58,26 +72,24 @@ static void init_layers_and_counter(const State& state) {
     goal_mem_layer.assign(g_goal.size(), -1);
     operator_mark.assign(g_operators.size(), false);
     
+    
     assert(action_mem_layer[g_operators.size()-1] == -1);
     assert(goal_mem_layer[g_goal.size()-1] == -1);
     assert(counter[g_operators.size() - 1] == 0);
-  
-    /*Init fact member layer for given state*/
-    for (int i = 0; i < g_variable_domain.size(); ++i) {
-        for (int j = 0; j < g_variable_domain[i]; ++j) {
-            if (state[i] == j) {
-                /* fact from search state. Check if it already contains an goal fact 
-                and init fact layer with 0 and schedule fact, as it is already achieved */
-                goal_fact(make_pair(i, j));
-                fac_mem_layer.insert(make_pair(make_pair(i, j), 0));
-                fact_schedule.push(make_pair(i, j));
-            }
-            else {
-                /*fact is not in search state and thus layer is initialized with infinity*/
-                fac_mem_layer.insert(make_pair(make_pair(i, j), -1));
-            }
+
+    for (int i = 0; i < g_variable_domain.size();i++) {
+        fac_mem_layer.insert(make_pair(make_pair(i, state[i]), 0));
+        //goal_fact(make_pair(i, state[i]));
+        fact_schedule.push(make_pair(i, state[i]));
+        /*Insert fact to goal layer if it is contained in the goal_set*/
+        if (goal_set.find(make_pair(i, state[i])) != goal_set.end()) {
+            goal_mem_map.insert(make_pair(make_pair(i, state[i]), timestep));
+            req_goal--;
         }
     }
+
+  
+
 
 }
 
@@ -93,6 +105,40 @@ static void update_action_layer_and_push_facts(int pos) {
         fact_schedule.push(make_pair(effects[e].var, effects[e].val));
         /*Update fact member layer*/
         fac_mem_layer.insert(make_pair(make_pair(effects[e].var, effects[e].val), timestep));
+    }
+}
+
+static void small_iteration() {
+    int q_size = fact_schedule.size();
+    for (int i = 0; i < q_size; ++i) {
+        pair <int, int >fact = fact_schedule.front();
+
+        /* Iterate over all actions preconditions*/
+        for (int op = 0; op < g_operators.size(); ++op) {
+            const vector<Condition>& preconditions = g_operators[op].get_preconditions();
+            if (!operator_mark[op]) {
+                assert(operator_mark[op] == false);
+                if (counter[op] < preconditions.size()) {
+                    /*Check whether action is already exhausted*/
+                    /*This should actually be improved, as the for-loop continues even if the fact is already found*/
+                    for (int m = 0; m < preconditions.size(); ++m) {
+                        if (preconditions[m].var == fact.first && preconditions[m].val == fact.second) {
+                            counter[op]++;
+                        }
+                    }
+                }
+                else if (counter[op] == preconditions.size()) {
+                    operator_mark[op] = true;
+                    assert(preconditions.size() == counter[op]);
+                    const vector<Effect>& effects = g_operators[op].get_effects();
+                    for (int e = 0; e < effects.size(); e++) {
+                        fact_schedule.push(make_pair(effects[e].var, effects[e].val));
+                    }
+                }
+            }
+
+        }
+        fact_schedule.pop();
     }
 }
 
@@ -123,8 +169,6 @@ static void layer_iteration() {
                 }
             }
 
-
-
         }
         fact_schedule.pop();
     }
@@ -152,36 +196,40 @@ static bool doStep() {
 
 
 int MaxHeuristic::compute_heuristic(const State &state) {
+    
     // TODO implementation
     timestep = 0;
     req_goal = g_goal.size();
-    cout << "Goals to achieve" << req_goal << endl;
+
+    //cout << "Goals to achieve" << req_goal << endl;
     
     init_layers_and_counter(state);
 
-    cout << "Goals to achieve that are not already achieved" << req_goal << endl;
-    if (test_goal(state)) {
-        return 0;
-   }
+    //cout << "Goals to achieve that are not already achieved" << req_goal << endl;
+
    
     bool progress = true;
-    
     while (progress) {
         //cout << "timestep " << timestep << " Goals to achieve before it " << req_goal << " Fact that are updated " << fact_schedule.size() << endl;
         progress = doStep();
 
     }
-    cout << "Goals that cannot be achieved" << req_goal << endl;
+    //cout << "Goals that cannot be achieved" << req_goal << endl;
    
     
     if (req_goal > 0) {
         return DEAD_END; /* Not all goal facts could be achieved*/
     }else {
-        assert(req_goal == 0);
-        int max = *max_element(goal_mem_layer.begin(),goal_mem_layer.end());
-        assert(max != -1);
-        assert(max != 0);
-        cout << "h-value " << max << endl;
+        //assert(req_goal == 0);
+        map <pair<int, int>, int>::iterator itr;
+        int max = -1;
+        for (itr = goal_mem_map.begin(); itr != goal_mem_map.end(); ++itr) {
+            if (itr->second > max) {
+                max = itr->second;
+            }
+
+        }
+        cout <<"req_goal " << req_goal << "  h-value " << max << endl;
         return max;
     }
 
